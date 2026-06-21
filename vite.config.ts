592a6b9601a7edb4235bc4bb40017bfef7aaecb6
Abhishek Dogne <abhidogne01@gmail.com>
2026-06-21 19:00:23 +0530

    add nitro plugin to vite.config.ts for serverless SSR mapping

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    nitro(),
     react(),
     tsconfigPaths(),
     tailwindcss(),
